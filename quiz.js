a2b2ed6ca204e6c674918d6c0b2ad4f663312e3d
Catelyn99 <katarzynasroka99@gmail.com>
2021-02-15 15:17:20 +0100

    added questions-elements

--- quiz.js
+++ quiz.js
@@ -4,6 +4,15 @@ const showElement = (currentElementSelector, nextElementSelector) => {
     document.querySelector(nextElementSelector).style.display = 'block';
 }
 
-document.querySelector('.button-start').addEventListener('click', function(){
-   showElement('.startsite', '.placeforquestion');
+document.querySelector('.button-start').addEventListener('click', function () {
+    showElement('.startsite', '.placeforquestions');
 });
+
+for (let q = 1; q < 5; q++) {
+    document.querySelector(`.question-${q} > .answers`).addEventListener('click', function () {
+        setTimeout(()=>{ 
+            showElement(`.question-${q}`, `.question-${q+1}`);
+        },650);
+    });
+}
+
