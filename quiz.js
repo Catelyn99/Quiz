59dbffc73220462da45213ab8d50e9f83f304f9a
Catelyn99 <katarzynasroka99@gmail.com>
2021-02-17 20:38:00 +0100

    added DOM content loaded

--- quiz.js
+++ quiz.js
@@ -1,71 +1,74 @@
-const showElement = (currentElementSelector, nextElementSelector) => {
-    document.querySelector(currentElementSelector).style.display = 'none';
-    document.querySelector(nextElementSelector).style.display = 'block';
-}
+window.addEventListener('DOMContentLoaded', (event) => {
 
-document.querySelector('.button-start').addEventListener('click', function () {
-    showElement('.startsite', '.placeforquestions');
-    this.style.display="none";
-});
-
-const onAnswerClick = (event) => {
-    const parent = event.target.parentElement;
-    const numberOfQuestion = Number(parent.parentElement.dataset.question);
-    if (numberOfQuestion) {
-        parent.style.background = "rgb(32, 110, 110)";
-        setTimeout(() => {
-            showElement(`.question-${numberOfQuestion}`, `.question-${numberOfQuestion + 1}`);
-        }, 650);
-    }
-}
-
-(() => {    
-    const answers = document.querySelectorAll(`.answers:not([data-question="5"])`);
-    for (let element of answers) {
-        element.addEventListener('click', onAnswerClick);
-    }
-})();
-
-const onClickLastQuestion = event => {
-    const countPoint = () => {
-        const inputs = document.querySelectorAll(`input`);
-        let checkAnswer = 0;
-        for (let element of inputs) {
-            element.disabled = true;
-            if (element.dataset.correctanswer === 'true' && element.checked) {
-                checkAnswer += 1;
-            }
-        }
-        document.querySelector(`#points`).innerHTML = checkAnswer;
+    const showElement = (currentElementSelector, nextElementSelector) => {
+        document.querySelector(currentElementSelector).style.display = 'none';
+        document.querySelector(nextElementSelector).style.display = 'block';
     }
 
-    const highlightCorrect = () => {
-        const correctAnswers = document.querySelectorAll(`input[data-correctanswer="true"]`);
-        for (let element of correctAnswers) {
-            element.parentElement.style.background = "rgb(24, 180, 94)";
-        }
-    };
+    document.querySelector('.button-start').addEventListener('click', function () {
+        showElement('.startsite', '.placeforquestions');
+        this.style.display = "none";
+    });
 
-    const showAllQuestions = () => {
-        event.target.style.background = "rgb(32, 110, 110)";
-        for (let q = 1; q <= 5; q++) {
-            document.querySelector(`.question-${q}`).style.display = "block";
+    const onAnswerClick = (event) => {
+        const parent = event.target.parentElement;
+        const numberOfQuestion = Number(parent.parentElement.dataset.question);
+        if (numberOfQuestion) {
+            parent.style.background = "rgb(32, 110, 110)";
+            setTimeout(() => {
+                showElement(`.question-${numberOfQuestion}`, `.question-${numberOfQuestion + 1}`);
+            }, 650);
         }
-    };
+    }
 
-    const removeEventListeners = () => {
+    (() => {
         const answers = document.querySelectorAll(`.answers:not([data-question="5"])`);
         for (let element of answers) {
-            element.removeEventListener('click', onAnswerClick);
+            element.addEventListener('click', onAnswerClick);
         }
-        document.querySelector(`.answers[data-question="5"]`).removeEventListener('click', onClickLastQuestion);
+    })();
+
+    const onClickLastQuestion = event => {
+        const countPoint = () => {
+            const inputs = document.querySelectorAll(`input`);
+            let checkAnswer = 0;
+            for (let element of inputs) {
+                element.disabled = true;
+                if (element.dataset.correctanswer === 'true' && element.checked) {
+                    checkAnswer += 1;
+                }
+            }
+            document.querySelector(`#points`).innerHTML = checkAnswer;
+        }
+
+        const highlightCorrect = () => {
+            const correctAnswers = document.querySelectorAll(`input[data-correctanswer="true"]`);
+            for (let element of correctAnswers) {
+                element.parentElement.style.background = "rgb(24, 180, 94)";
+            }
+        };
+
+        const showAllQuestions = () => {
+            event.target.style.background = "rgb(32, 110, 110)";
+            for (let q = 1; q <= 5; q++) {
+                document.querySelector(`.question-${q}`).style.display = "block";
+            }
+        };
+
+        const removeEventListeners = () => {
+            const answers = document.querySelectorAll(`.answers:not([data-question="5"])`);
+            for (let element of answers) {
+                element.removeEventListener('click', onAnswerClick);
+            }
+            document.querySelector(`.answers[data-question="5"]`).removeEventListener('click', onClickLastQuestion);
+        }
+
+        document.querySelector('.result').style.display = "block";
+        countPoint();
+        showAllQuestions();
+        highlightCorrect();
+        removeEventListeners();
     }
 
-    document.querySelector('.result').style.display = "block";
-    countPoint();
-    showAllQuestions();
-    highlightCorrect();
-    removeEventListeners();
-}
-
-document.querySelector('.answers[data-question="5"]').addEventListener('click', onClickLastQuestion);
+    document.querySelector('.answers[data-question="5"]').addEventListener('click', onClickLastQuestion);
+});
